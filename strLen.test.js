de10173f50d141c6c2ac467c000fddebd38d6ad1
Trictonicmp <code_shadow@hotmail.com>
2022-05-03 13:42:11 -0500

    Test a class

--- strLen.test.js
+++ strLen.test.js
@@ -26,3 +26,57 @@ test('length of string 1234567890 should be 10', () => {
 test('text abcd should return dcba', () => {
   expect(reverseStr('abcd')).toBe('dcba');
 });
+
+
+//Arrange
+const Calculator = require('./Calculator.js');
+let calculator = new Calculator();
+
+describe('add', () => {
+  test('5 + 3 sould be equal to 8', () => {
+    expect(calculator.add(5,3)).toBe(8);
+  });
+  test('-5 + 4 sould be equal to -1', () => {
+    expect(calculator.add(-5,4)).toBe(-1);
+  });
+  test('-5 + 8 sould be equal to 3', () => {
+    expect(calculator.add(-5,8)).toBe(3);
+  });
+});
+
+describe('substract', () => {
+  test('5 - 3 sould be equal to 2', () => {
+    expect(calculator.substract(5,3)).toBe(2);
+  });
+  test('-5 - 4 sould be equal to -9', () => {
+    expect(calculator.substract(-5,4)).toBe(-9);
+  });
+  test('5 - 8 sould be equal to -3', () => {
+    expect(calculator.substract(5,8)).toBe(-3);
+  });
+});
+
+describe('divide', () => {
+  test('5 / 3 sould be close to 1.66', () => {
+    expect(calculator.divide(5,3)).toBeCloseTo(1.6666);
+  });
+  test('5 / 4 sould be close to 1.25', () => {
+    expect(calculator.divide(5,4)).toBeCloseTo(1.25);
+  });
+  test('5 / -8 sould be close to -0.625', () => {
+    expect(calculator.divide(5,-8)).toBe(-0.625);
+  });
+});
+
+
+describe('multiply', () => {
+  test('5 * 3 sould be equal to 15', () => {
+    expect(calculator.multiply(5,3)).toBe(15);
+  });
+  test('5 * -4 sould be equal to -20', () => {
+    expect(calculator.multiply(5,-4)).toBe(-20);
+  });
+  test('5 * 8 sould be equal to 40', () => {
+    expect(calculator.multiply(5,8)).toBe(40);
+  });
+});
